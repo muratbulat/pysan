c09ac6f4c0bd91fa7be5006934b88c1a3de359f5
Murat Bulat <git@mbulat.com>
2023-05-03 22:23:48 +0300

    pyvim

--- vcenter/vmip_to_vmatt.py
+++ vcenter/vmip_to_vmatt.py
@@ -1,57 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from com.vmware.vcenter.vm_client import CustomAttributes
-from com.vmware.vcenter.vm_client import Network
-from vmware.vapi.vsphere.client import create_vsphere_client
-from pyVim.connect import SmartConnectNoSSL, Disconnect
+from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
 
 # Connect to vCenter server
-client = create_vsphere_client(server="<VCENTER_SERVER_IP>", username="<USERNAME>", password="<PASSWORD>")
+si = SmartConnect(host="<VCENTER_SERVER_IP>", user="<USERNAME>", pwd="<PASSWORD>")
 
-# Get the network information of all virtual machines
-network = client.vcenter.vm.Network
-vms = client.vcenter.VM.list()
-
-# Connect to vCenter server using PyVmomi
-si = SmartConnectNoSSL(host="<VCENTER_SERVER_IP>", user="<USERNAME>", pwd="<PASSWORD>")
+# Get all virtual machines
 content = si.RetrieveContent()
+vm_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
+vms = vm_view.view
 
-# Define the custom attribute key and category
+# Define the attribute key
 attribute_key = "ip_address"
-category_name = "Custom Attributes"
 
-# Get the category ID
-category_id = None
-category_list = client.vcenter.vm_customization.Spec.list_categories()
-for category in category_list:
-    if category.name == category_name:
-        category_id = category.category_id
-        break
-
-if category_id is None:
-    print(f"Custom attribute category '{category_name}' not found")
-    exit()
-
-# Iterate through all virtual machines and set their IP addresses as custom attributes
+# Iterate through all virtual machines and set their IP addresses as attributes
 for vm in vms:
     vm_name = vm.name
     ip_address = None
-    for nic_info in network.list(vm.vm):
-        if nic_info.nic_type == "VMXNET3":
-            nic = nic_info.nic
-            if nic.backing.network_type == "STANDARD_PORTGROUP":
-                ip_address = nic.ip_address.ip_address
+    for nic in vm.guest.net:
+        if nic.network != None:
+            ip_address = nic.ipAddress[0]
     if ip_address:
-        vm_object = content.searchIndex.FindByUuid(None, vm.vm, True, False)
-        if vm_object is None:
-            print(f"Virtual machine '{vm_name}' not found")
-            continue
-        else:
-            print(f"Setting custom attribute '{attribute_key}' to '{ip_address}' for virtual machine '{vm_name}'")
-            custom_attributes = CustomAttributes(client)
-            spec = CustomAttributes.CreateSpec(category_id=category_id, key=attribute_key, value=ip_address)
-            custom_attributes.create(vm_object, spec)
-
-# Disconnect from vCenter server using PyVmomi
+        print(f"Setting attribute '{attribute_key}' to '{ip_address}' for virtual machine '{vm_name}'")
+        spec = vim.vm.ConfigSpec()
+        spec.extraConfig = [vim.option.OptionValue(key=attribute_key, value=ip_address)]
+        vm.ReconfigVM_Task(spec=spec)
+        
+# Disconnect from vCenter server
 Disconnect(si)
